f86bd0b7fe9c7c3c0bf256955fe57d0504646890
Ayat Maulana <ayatmaulana.04@gmail.com>
2021-04-01 20:37:35 +0700

    wip 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,51 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:overlay_support/overlay_support.dart';
+import 'package:window_size/window_size.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:ping_discover_network/ping_discover_network.dart';
+// import 'package:connectivity/connectivity.dart';
+// import 'package:flutter_ip/flutter_ip.dart';
+import 'package:dartspatcher/dartspatcher.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowFrame(Rect.fromLTRB(1200.0, 500.0, 1800.0, 1125.0));
+    setWindowMinSize(const Size(1280, 500));
+    setWindowMaxSize(Size.infinite);
+  }
+
+  Dartspatcher dartspatcher = Dartspatcher();
+  dartspatcher.locals['var'] = 'value';
+
+  dartspatcher.setVirtualDirectory('web');
+
+  dartspatcher.get('/', [
+    (HttpRequest request, Map<String, dynamic> params, Function next,
+        [Map<dynamic, dynamic>? locals]) {
+      print("fire");
+      request.response.close();
+    }
+  ], {
+    'var': 'value'
+  });
+
+  dartspatcher.setMiddleware([
+    (HttpRequest request, Map<String, dynamic> params, Function next,
+        [Map<dynamic, dynamic>? locals]) {
+      print('middlware 2');
+    }
+  ]);
+
+  dartspatcher.setErrorHandler((HttpRequest request, dynamic e, StackTrace s) {
+    print('Error Handler');
+    dartspatcher.close(request, HttpStatus.internalServerError);
+  });
+
+  dartspatcher.listen(InternetAddress.loopbackIPv4, 4040, (HttpServer server) {
+    print('Listening on localhost:${server.port}');
+  });
   runApp(MyApp());
 }
 
@@ -8,21 +53,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return OverlaySupport(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.green,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -68,46 +115,172 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title!),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Color(0xFF0E2C4F),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                  ),
+                  buildMenuItem(isActive: true),
+                  buildMenuItem(),
+                  buildMenuItem()
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Color(0xFFFFFFFF),
+              child: Scrollbar(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                      buildList(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+
+  void analyze() async {
+    var ips = await NetworkInterface.list();
+    var ip = ips[0].addresses[0].address;
+    final String subnet = ip.substring(0, ip.lastIndexOf('.'));
+    // remote address pkg installer port default
+    final int port = 12800;
+
+    final stream = NetworkAnalyzer.discover2(subnet, port);
+    stream.listen((NetworkAddress addr) {
+      if (addr.exists) {
+        print('Found device: ${addr.ip}');
+      }
+    });
+  }
+
+  Container buildMenuItem({bool isActive = false}) {
+    return Container(
+      decoration: isActive
+          ? BoxDecoration(
+              border: Border(left: BorderSide(color: Colors.white, width: 3)))
+          : null,
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(
+            Icons.airplay,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Text(
+            "Courses",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          )
+        ],
+      ),
+    );
+  }
+
+  void onClick() {
+    analyze();
+    // popup a toast.
+    toast('Hello world!');
+
+    // show a notification at top of screen.
+    showSimpleNotification(Text("this is a message from simple notification"),
+        background: Colors.green, position: NotificationPosition.bottom);
+  }
+
+  Widget buildList() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Color(0xFFF1F1F1), width: 3)),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(flex: 1, child: Icon(Icons.access_alarm)),
+              Expanded(
+                flex: 6,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Changing Lens - Large Vehicele",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        buildSubList(),
+                        buildSubList(),
+                        buildSubList(),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                  flex: 2,
+                  child:
+                      FlatButton(onPressed: onClick, child: Text("Press Me"))),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Container buildSubList() {
+    return Container(
+      child: Row(
+        children: [
+          Icon(
+            Icons.alarm_outlined,
+            color: Colors.cyan,
+          ),
+          Text(
+            "Access Expires in 20 hour",
+            style: TextStyle(fontSize: 10),
+          )
+        ],
+      ),
     );
   }
 }
